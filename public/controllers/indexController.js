983a9bedf112a92f25e092122a539074ab911166
ribeiroluis <ribeiro.luis@hotmail.com>
2015-08-13 09:31:29 -0300

    express route

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -1,8 +1,8 @@
 /// <reference path="../js/jquery-1.11.2.min.js" />
 
-window.app.controller("indexController", function ($scope) {
+window.app.controller("indexController", function ($scope, $http) {
 	"use strict",
-	
+
 	$scope.headerSrc = "tmpl/header.html";
 	$scope.footerSrc = "tmpl/footer.html";
 	$scope.loadingTmp = "tmpl/loading.html";
@@ -12,6 +12,43 @@ window.app.controller("indexController", function ($scope) {
 	$scope.selectMenuItem = function (value) {
 		$(value.currentTarget).siblings('.active').toggleClass('active');
 	}
+
+	$http.get('/user')
+		.success(function (data) {
+			console.info(data);
+		})
+		.error(function (data) {
+			console.log('Error: ' + data);
+		});
+	$http.get('/product')
+		.success(function (data) {
+			console.info(data);
+		})
+		.error(function (data) {
+			console.log('Error: ' + data);
+		});
+	$http.get('/client')
+		.success(function (data) {
+			console.info(data);
+		})
+		.error(function (data) {
+			console.log('Error: ' + data);
+		});
+	$http.post('/user/' + $scope.appTitle)
+		.success(function (data) {
+			console.info(data);
+		})
+		.error(function (data) {
+			console.log('Error: ' + data);
+		});
+	$http.post('/client/' + $scope.appTitle)
+		.success(function (data) {
+			console.info(data);
+		})
+		.error(function (data) {
+			console.log('Error: ' + data);
+		});
+
 	$scope.checkAuth = function () {
 		if ($scope.isAuth)
 			return true;
