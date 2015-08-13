84fe52e8755e176bff999f2ecf83656531911cb9
ribeiroluis <ribeiro.luis@hotmail.com>
2015-08-13 08:38:04 -0300

    inital commit

---
+++ public/app.js
@@ -0,0 +1,18 @@
+﻿
+var app = angular.module('ilumicar', ['ngResource', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'homeController'
+    }).when('/report', {
+        templateUrl: 'views/report.html',
+        controller: 'reportController'
+   })//.otherwise({
+    //     templateUrl: 'tmpl/login.html',
+    //     controller: 'loginController'
+    // });
+    .otherwise({
+    		redirectTo : '/home'
+    	});
+});
---
+++ public/controllers/indexController.js
@@ -0,0 +1,19 @@
+/// <reference path="../js/jquery-1.11.2.min.js" />
+
+window.app.controller("indexController", function ($scope) {
+	"use strict",
+	
+	$scope.headerSrc = "tmpl/header.html";
+	$scope.footerSrc = "tmpl/footer.html";
+	$scope.loadingTmp = "tmpl/loading.html";
+	$scope.appTitle = "Ilumicar";
+	$scope.isAuth = false;
+
+	$scope.selectMenuItem = function (value) {
+		$(value.currentTarget).siblings('.active').toggleClass('active');
+	}
+	$scope.checkAuth = function () {
+		if ($scope.isAuth)
+			return true;
+	}
+});
